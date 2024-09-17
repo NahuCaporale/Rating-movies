1c2dabec2f0aaf9e7294c0ee2cd0f31a19cf0ffd
Nahu Caporale <nahucaporale21@gmail.com>
2024-09-16 22:00:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+ # Rating de films y tv-shows
+La idea de el sitio es la creacion de un usuario el cual pueda hacer valoraciones de tv-shows o peliculas y sean guardadas en una DB. Las entidades son shows,reviews y el usuario.
+La tabla shows está relacionada con la tabla reviews a través de la columna id, que se conecta a show_id en la tabla de reseñas, con lo que asi podriamos mostrar el show/film que fue reseñado y a la vez user se relaciona
+a traves de el id de user y la clave foranea user_id en la tabla reviews, por lo que con esta relacion de 3 tablas podemos ver que show fue reseñado y porque usuario(se puede mostrar el username y el id de el mismo)
+ # Diagrama de relaciones
+(Diagrama relaciones_db.png)
+ # Integrantes
+-Nahuel Caporale
+-Geronimo Moroni
---
+++ rateur_fitv.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-09-2024 a las 23:58:55
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rateur_fitv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `show_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `review_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `show_id`, `user_id`, `rating`, `review_date`) VALUES
+(1, 1, 2, 5, '2024-09-15'),
+(3, 2, 1, 5, '2024-09-15'),
+(4, 3, 2, 5, '2024-09-15');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `shows`
+--
+
+CREATE TABLE `shows` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `genre` varchar(100) NOT NULL,
+  `release_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `shows`
+--
+
+INSERT INTO `shows` (`id`, `title`, `genre`, `release_date`) VALUES
+(1, 'Shutter island', 'Psychological thriller', '2010-02-18'),
+(2, 'Paths of Glory', 'Action', '1957-12-20'),
+(3, '12 Angry Men', 'Suspense/Crime', '1957-11-07');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `user`, `password`, `email`) VALUES
+(1, 'nau', '', 'nahucaporale21@gmail.com'),
+(2, 'tomi2k', '', 'tomi2k21@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `show_id` (`show_id`);
+
+--
+-- Indices de la tabla `shows`
+--
+ALTER TABLE `shows`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `user` (`user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `shows`
+--
+ALTER TABLE `shows`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`show_id`) REFERENCES `shows` (`id`),
+  ADD CONSTRAINT `reviews_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
